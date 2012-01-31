e8c20aec37df3d89c3d0864600cf6ccccebf5949
Camille Reynders <camille.reynders@ibbt.be>
2012-01-31 11:02:35 +0100

    swapped dictionary for object

--- test/dijon.InjectorTests.js
+++ test/dijon.InjectorTests.js
@@ -28,125 +28,81 @@ module( 'dijon.Injector', {
 	}
 });
 
-test( 'unnamed mapSingleton', function(){
-	injector.mapSingleton( TestClassA );
-	ok( injector.hasMapping( TestClassA ) );
+test( 'mapSingleton', function(){
+	injector.mapSingleton( 'a', TestClassA );
+	ok( injector.hasMapping( 'a' ) );
 })
 
-test( 'unnamed mapValue', function(){
+test( 'mapValue', function(){
 	var a = new TestClassA();
-	injector.mapValue( TestClassA, a );
-	ok( injector.hasMapping( TestClassA ) );
+	injector.mapValue( 'a', a );
+	ok( injector.hasMapping( 'a' ) );
 })
 
-test( 'unnamed mapClass', function(){
-	injector.mapClass( TestClassA, function(){} );
-	ok( injector.hasMapping( TestClassA ) );
+test( 'mapClass', function(){
+	injector.mapClass( 'a', function(){} );
+	ok( injector.hasMapping( 'a' ) );
 })
 
-test( 'unnamed mapSingletonOf', function(){
-	injector.mapSingletonOf( TestClassA, function(){} );
-	ok( injector.hasMapping( TestClassA ) );
-})
-
-test( 'getInstance for unnamed mapSingleton', function(){
-	injector.mapSingleton( TestClassA );
-	var a = injector.getInstance( TestClassA );
-	ok( a instanceof TestClassA );
-	strictEqual( injector.getInstance( TestClassA ), a );
-})
-
-test( 'getInstance for unnamed mapSingletonOf', function(){
-	injector.mapSingletonOf( TestClassA, TestClassB );
-	var b = injector.getInstance( TestClassA );
-	ok( b instanceof TestClassB );
-	strictEqual( injector.getInstance( TestClassA ), b );
+test( 'getInstance for mapSingleton', function(){
+	injector.mapSingleton( 'a', TestClassA );
+    var inj = injector;
+	var a = injector.getInstance( 'a' );
+	ok( a instanceof TestClassA, 'must be instance of TestClassA' );
+	strictEqual( injector.getInstance( 'a' ), a, 'must be single instance' );
 })
 
 test( 'getInstance for mapClass', function(){
-	injector.mapClass( TestClassA, TestClassB );
-	var b = injector.getInstance( TestClassA );
-	ok( b instanceof TestClassB );
-	notStrictEqual( injector.getInstance( TestClassA ), b );
+	injector.mapClass( 'a', TestClassA );
+	var a = injector.getInstance( 'a' );
+	ok( a instanceof TestClassA );
+	notStrictEqual( injector.getInstance( 'a' ), a );
 })
 
 test( 'getInstance for mapValue', function(){
 	var a = new TestClassA();
-	injector.mapValue( TestClassA, a );
-	strictEqual( injector.getInstance( TestClassA ), a );
+	injector.mapValue( 'a', a );
+	strictEqual( injector.getInstance( 'a' ), a );
 })
 
 test( 'instantiate for mapSingleton', function(){
-	injector.mapSingleton( TestClassA );
-	var a = injector.getInstance( TestClassA );
-	notStrictEqual( injector.instantiate( TestClassA ), a );
+	injector.mapSingleton( 'a', TestClassA );
+	var a = injector.getInstance( 'a' );
+	notStrictEqual( injector.instantiate( 'a' ), a, 'instantiate must always provide a new instance regardless of rules' );
 })
 
-test( 'instantiate for mapSingletonOf', function(){
-	injector.mapSingletonOf( TestClassA, TestClassB );
-	var b = injector.getInstance( TestClassA );
-	notStrictEqual( injector.instantiate( TestClassA ), b );
-})
 
 test( 'instantiate for mapClass', function(){
-	injector.mapClass( TestClassA, TestClassB );
-	var b = injector.getInstance( TestClassA );
-	notStrictEqual( injector.instantiate( TestClassA ), b );
+	injector.mapClass( 'a', TestClassA );
+	var a = injector.getInstance( 'a' );
+	notStrictEqual( injector.instantiate( 'a' ), a );
 })
 
 test( 'instantiate for mapValue', function(){
 	var a = new TestClassA();
-	injector.mapValue( TestClassA, a );
-	notStrictEqual( injector.instantiate( TestClassA ), a );
+	injector.mapValue( 'a', a );
+	notStrictEqual( injector.instantiate( 'a' ), a );
 })
 
-test( 'unnamed unmap', function(){
-	injector.mapSingleton( TestClassA )
-	injector.unmap( TestClassA );
-	ok( ! injector.hasMapping( TestClassA ) );
+test( 'unmap', function(){
+	injector.mapSingleton( 'a', TestClassA )
+	injector.unmap( 'a' );
+	ok( ! injector.hasMapping( 'a' ) );
 })
 
 test( 'addOutlet', function(){
-	injector.mapSingleton( TestClassA );
-	injector.mapSingleton( TestClassB );
-	injector.addOutlet( TestClassB, 'bar', TestClassA );
-	var b = injector.getInstance( TestClassB );
+	injector.mapSingleton( 'a', TestClassA );
+	injector.mapSingleton( 'b', TestClassB );
+	injector.addOutlet( 'a', 'b', 'bar' );
+	var b = injector.getInstance( 'b' );
 	ok( b.bar instanceof TestClassA );
 })
 
 test( 'removeOutlet', function() {
-	injector.mapSingleton( TestClassA );
-	injector.mapSingleton( TestClassB );
-	injector.addOutlet( TestClassB, 'bar', TestClassA );
-	injector.removeOutlet( TestClassB, 'bar' );
-	var b = injector.getInstance( TestClassB );
+	injector.mapSingleton( 'a', TestClassA );
+	injector.mapSingleton( 'b', TestClassB );
+	injector.addOutlet( 'a', 'b', 'bar' );
+	injector.removeOutlet( 'b', 'bar' );
+	var b = injector.getInstance( 'b' );
 	strictEqual( b.bar, undefined );
 })
-
-test( 'autoexec setup', function(){
-	var isExecuted = 0;
-	function TestActor(){
-	}
-	TestActor.prototype = {
-		setup : function(){
-			isExecuted++;
-		}
-	}
-	injector.mapSingleton( TestActor );
-	var a = injector.getInstance( TestActor );
-	equal( 1, isExecuted );
-})
-
-test( 'covariant injection', function(){
-	var isExecuted = 0;
-	function TestActor(){
-	}
-	TestActor.prototype = new TestClassB();
-	TestActor.prototype.constructor = TestActor;
-	injector.addOutlet( TestClassB, 'bar', TestClassA );
-	injector.mapSingleton( TestClassA );
-	injector.mapSingleton( TestActor );
-	var a = injector.getInstance( TestActor );
-	ok( a.bar instanceof TestClassA  );
-
-} )
